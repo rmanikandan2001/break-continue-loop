271ffe04236d01a3e87755476edd4ea72f0d3ac1
rmanikandan2001 <rmanikandan6867@gmail.com>
2023-08-25 04:06:28 -0700

    add

---
+++ main.js
@@ -0,0 +1,23 @@
+// For break
+
+for(let i=1; i<=5; i++)
+{
+	if(i==4)
+	{
+		break;
+	}
+	document.write(i);
+	document.write("<br>");
+}
+
+// For continue
+
+for(let i=1; i<=5; i++)
+{
+	if(i==3)
+	{
+		continue;
+	}
+	document.write(i);
+	document.write("<br>");
+}
